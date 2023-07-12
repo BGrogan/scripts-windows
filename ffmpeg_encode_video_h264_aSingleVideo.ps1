5659399537d1709c13a4827b4283ab88ae69aee3
Brian Grogan <brian.rg@gmail.com>
2023-07-11 21:44:00 -0600

    Prod Hotfix ffmpeg video
    Hotfixes prod to work

--- ffmpeg_encode_video_h264_aSingleVideo.ps1
+++ ffmpeg_encode_video_h264_aSingleVideo.ps1
@@ -1,10 +1,10 @@
 $input_video_name = $args[0]
-if ($path_to_files_to_process -eq $null -OR $path_to_files_to_process -eq "") {
+if ($input_video_name -eq $null -OR $input_video_name -eq "") {
 	write-host "Argument error!
-	Please specify a single video file. 
-	Only this one video will be processed in this script."
+	Please specify a single video file. Did not find anything at ($input_video_name).
+	Only the individual video will be processed in this script."
 	exit
 }
 
-$output = $input_video_name_ffmpeg + "_ffmpeg.mp4" 
+$output = $input_video_name + "_ffmpeg.mp4" 
 ffmpeg -i "$input_video_name" -codec:v libx264 -preset ultrafast -force_key_frames "expr:gte (t,n_forced*4)" -hls_time 4 -hls_playlist_type vod -hls_segment_type mpegts $output
